d41f2ec3aecdc681d1b8368da590fc3914f9a213
Adman <admanabid@gmail.com>
2025-05-24 18:08:06 +0600

    config1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
